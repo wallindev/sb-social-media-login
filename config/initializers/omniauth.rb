6b4790e74e9731940ff75c611653b076e536abfa
Mikael Wallin <wallindev@gmail.com>
2021-03-16 08:25:45 +0100

    Cleaned up

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,14 +4,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook,
 	Rails.application.credentials.dig(:development, :FACEBOOK_APP_ID),
 	Rails.application.credentials.dig(:development, :FACEBOOK_APP_SECRET),
-	redirect_uri: 'https://sb-social-media-login.herokuapp.com/auth/google_oauth2/callback',
 	:provider_ignores_state => true
 	
 	provider :google_oauth2,
 	Rails.application.credentials.dig(:development,	:GOOGLE_CLIENT_ID),
 	Rails.application.credentials.dig(:development, :GOOGLE_CLIENT_SECRET)#,
-	# scope: 'userinfo.email,userinfo.profile',
-	# redirect_uri: 'https://localhost:3000/auth/google_oauth2/callback',
-	# :provider_ignores_state => true,
-	# :skip_jwt => true
+	# :provider_ignores_state => true
 end
